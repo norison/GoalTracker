814ceb39526f03732ea592b83caaafde1a681f7b
Ihor Matiev <ihor.matiev@gmail.com>
2024-01-30 10:48:29 +0200

    Renamed projects

--- src/GoalTracker.UI/App.xaml.cs
+++ src/GoalTracker.UI/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace SimpleBudget.UI;
+namespace GoalTracker.UI;
 
 /// <summary>
 /// Interaction logic for App.xaml
--- src/GoalTracker.UI/MainWindow.xaml.cs
+++ src/GoalTracker.UI/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SimpleBudget.UI;
+namespace GoalTracker.UI;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
